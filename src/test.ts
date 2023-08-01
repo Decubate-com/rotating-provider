188fa53facb8a0c08a1ddf86c8c55e5466259e03
0xask3 <grimreapergit619@gmail.com>
2023-08-01 15:41:35 +0530

    feat: 🎸 Fix lint

--- src/test.ts
+++ src/test.ts
@@ -9,13 +9,16 @@ const provider = testClass.provider.provider;
 
 function main() {
 	let counter = 0;
+	// eslint-disable-next-line @typescript-eslint/no-misused-promises
 	setInterval(async () => {
 		const block = await provider.getBlockNumber();
 		counter++;
 		if (counter === 5) {
 			testClass.provider.stopRotation();
+			// eslint-disable-next-line n/no-process-exit
 			process.exit(0);
 		}
+
 		console.log("Current block:", block);
 	}, 3000);
 }
